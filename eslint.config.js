dd73ff5ec74f1d5bb6fd6da68580f137110ff39f
Kathy <kathy.kao@xuenn.com>
2025-02-12 12:52:24 +0800

    update prettier setting on eslint.config.js

--- eslint.config.js
+++ eslint.config.js
@@ -1,7 +1,7 @@
-import globals from "globals";
-import pluginJs from "@eslint/js";
-import eslintConfigPrettier from "eslint-config-prettier";
-import eslintPluginPrettier from "eslint-plugin-prettier";
+import globals from 'globals';
+import pluginJs from '@eslint/js';
+import eslintConfigPrettier from 'eslint-config-prettier';
+import eslintPluginPrettier from 'eslint-plugin-prettier';
 
 /** @type {import('eslint').Linter.FlatConfig[]} */
 export default [
@@ -13,24 +13,34 @@ export default [
       js: pluginJs,
       prettier: eslintPluginPrettier,
     },
-    ignores: ["public", "dist", "node_modules"],
+    ignores: ['public', 'dist', 'node_modules'],
     rules: {
-      "no-console": "off",
-      "no-promise-executor-return": "off",
-      "no-plusplus": "off",
-      "no-loop-func": "off",
-      "array-callback-return": "error",
-      "object-curly-newline": "off",
-      "max-len": ["error", { code: 140 }],
+      'no-console': 'off',
+      'no-promise-executor-return': 'off',
+      'no-plusplus': 'off',
+      'no-loop-func': 'off',
+      'array-callback-return': 'error',
+      'object-curly-newline': 'off',
+      'max-len': ['error', { code: 140 }],
       // eslint v8.53.0 之後的版本，棄用格式化規則
       // quotes: ['error', 'single'],
-      "arrow-body-style": "off",
-      "import/extensions": "off",
-      "import/prefer-default-export": "off",
-      "implicit-arrow-linebreak": "off",
-      "no-unused-vars": "off",
+      'arrow-body-style': 'off',
+      'import/extensions': 'off',
+      'import/prefer-default-export': 'off',
+      'implicit-arrow-linebreak': 'off',
+      'no-unused-vars': 'off',
       // Add Prettier as a rule
-      "prettier/prettier": "error",
+      'prettier/prettier': [
+        'error',
+        {
+          tabWidth: 2,
+          semi: true,
+          printWidth: 120,
+          singleQuote: true,
+          trailingComma: 'all',
+          endOfLine: 'auto',
+        },
+      ],
     },
   },
 ];
